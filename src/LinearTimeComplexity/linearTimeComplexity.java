0429779f0724ddfe0f64d99db0c1b26e636546e7
pankajkoree <pankajkori2057@gmail.com>
2024-11-15 21:28:01 +0530

    linear time complexity

--- src/LinearTimeComplexity/linearTimeComplexity.java
+++ src/LinearTimeComplexity/linearTimeComplexity.java
@@ -1,6 +1,8 @@
+package LinearTimeComplexity;
+
 public class linearTimeComplexity {
     public static void main(String[] args){
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        String[] cars = new String[]{"Volvo", "BMW", "Ford", "Mazda"};
         for (int i = 0; i < cars.length; i++) {
             System.out.println(cars[i]);
         }
