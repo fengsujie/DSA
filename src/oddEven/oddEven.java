f2af9236677b8ae0a2fa6a029527fa854b9c0ff2
pankajkoree <pankajkori2057@gmail.com>
2024-11-21 22:58:09 +0530

    odd even

---
+++ src/oddEven/oddEven.java
@@ -0,0 +1,13 @@
+package oddEven;
+import java.util.Scanner;
+public class oddEven {
+    public static void main(String[] args) {
+        Scanner inputObject = new Scanner(System.in);
+        System.out.print("Enter the number");
+        int num = inputObject.nextInt();
+
+
+    }
+
+
+}
