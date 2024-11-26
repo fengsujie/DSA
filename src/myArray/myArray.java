3ee10cdcbd969671819dc0a04e96e213e3e0e66a
pankajkoree <pankajkori2057@gmail.com>
2024-11-26 22:54:31 +0530

    toString

--- src/myArray/myArray.java
+++ src/myArray/myArray.java
@@ -12,4 +12,18 @@ public void insert(int value){
     this.items[currentIndex]=value;
     currentIndex++;
 }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder("[");
+
+        for (int i = 0; i < currentIndex; i++) {
+            str.append(items[i]);
+            if (i < currentIndex-1) {
+                str.append(", ");
+            }
+        }
+
+        str.append("]");
+        return str.toString();
+    }
 }
