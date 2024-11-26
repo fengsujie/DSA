f43751052d62b0af983848ce898fa31b452c0891
pankajkoree <pankajkori2057@gmail.com>
2024-11-26 23:22:55 +0530

    operations of array

--- src/myArray/Main.java
+++ src/myArray/Main.java
@@ -11,5 +11,24 @@ public class Main {
 
 
     System.out.println(ob);
+
+    System.out.println("Index of the value is : "+ob.indexOf(33));
+
+    System.out.println(ob.removeAt(2));
+    System.out.println("after removing the element : "+ob);
+
+    System.out.println("Maximum value is : "+ob.max());
+
+    System.out.println("Minimum value is : "+ob.min());
+    ob.reverse();
+    System.out.println("Reversed array : " +ob);
+
+
+        ob.insert(55);
+        ob.insert(66);
+        ob.insert(77);
+        ob.insert(88);
+
+        System.out.println(ob);
     }
 }
--- src/myArray/myArray.java
+++ src/myArray/myArray.java
@@ -9,21 +9,92 @@ public myArray(int initialize){
 }
 
 public void insert(int value){
+    if(currentIndex==items.length){
+        int[] temp=new int[items.length*2];
+        for(int i=0;i<currentIndex;i++){
+            temp[i]=items[i];
+        }
+        items =temp;
+    }
     this.items[currentIndex]=value;
     currentIndex++;
 }
 
-    public String toString() {
-        StringBuilder str = new StringBuilder("[");
+public String toString() {
+    StringBuilder str = new StringBuilder("[");
 
-        for (int i = 0; i < currentIndex; i++) {
-            str.append(items[i]);
-            if (i < currentIndex-1) {
-                str.append(", ");
-            }
+    for (int i = 0; i < currentIndex; i++) {
+        str.append(items[i]);
+        if (i < currentIndex-1) {
+            str.append(", ");
         }
-
-        str.append("]");
-        return str.toString();
     }
+
+    str.append("]");
+    return str.toString();
+}
+
+// function to find the IndexOf value
+    public int indexOf(int value){
+    for(int i=0;i<currentIndex;i++){
+        if(items[i]==value){
+            return i;
+        }
+    }
+    return -1;
+    }
+
+//    function to remove item using index
+public boolean removeAt(int index) {
+    if (index >= currentIndex || index < 0) {
+        throw new IllegalArgumentException("Invalid index: " + index);
+    }
+
+    // Shift elements to the left, overwriting the element at the given index
+    for (int i = index; i < currentIndex - 1; i++) {
+        items[i] = items[i + 1];
+    }
+
+    currentIndex--;
+    return true;
+}
+
+// maximum item in array
+    public int max(){
+    int result = items[0];
+    for(int i=0;i<currentIndex;i++){
+        if(items[i]>result){
+            result = items[i];
+        }
+    }
+    return result;
+
+    }
+
+//    minimum item in array
+public int min(){
+    int result = items[0];
+    for(int i=0;i<currentIndex;i++){
+        if(items[i]<result){
+            result = items[i];
+        }
+    }
+    return result;
+
+}
+
+//reversing the array
+    public void reverse(){
+    int i=0;
+    int j=currentIndex-1;
+    while(i<j){
+        int temp=items[i];
+        items[i]=items[j];
+        items[j]=temp;
+        i++;
+        j--;
+    }
+    }
+
+
 }
