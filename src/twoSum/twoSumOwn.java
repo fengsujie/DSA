f98c1c2e8db184d3ba8158a19764e259ead9eda1
pankajkoree <pankajkori2057@gmail.com>
2024-11-27 21:53:36 +0530

    not the optimized one, coz its time complexity is O(n^2)

---
+++ src/twoSum/twoSumOwn.java
@@ -0,0 +1,16 @@
+package twoSum;
+
+
+public class twoSumOwn {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,7,11,15};
+        int target=9;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println("["+i+" , "+j+"]");
+                }
+            }
+        }
+    }
+}
